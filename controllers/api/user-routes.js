d5b6d75f90a7689e2556390f983e4b1a48aa46dd
shiba inu <jefferson121996@gmail.com>
2022-06-19 15:36:50 -0400

    login and logout created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -5,7 +5,7 @@ const { User, Post, Vote, Comment } = require('../../models');
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll({
-        attributes: { exclude: ['password'] }
+        // attributes: { exclude: ['password'] }
 
     })
         .then( dbUserData => res.json(dbUserData))
@@ -66,11 +66,20 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        // .then(dbUserData => res.json(dbUserData))
+        // .catch(err => {
+        //     console.log(err);
+        //     res.status(500).json(err);
+        // });
 });
 
 router.post('/login', (req, res) => {
@@ -91,9 +100,29 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });    
+        })
+
+        
 
     });
+
+});
+
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 
 });
 
